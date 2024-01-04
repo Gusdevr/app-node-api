b4888caf6bb18c470003fc3a71309337dfdb2980
unknown <gustav.y@hotmail.com>
2024-01-04 10:44:26 -0300

    struture

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-// src/server.ts
+
 import express from 'express';
 import mustacheExpress from 'mustache-express';
 import path from 'path';
@@ -8,7 +8,6 @@ import apiRoutes from './routes/api';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Configurar o Mustache como template engine
 app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache');
 app.set('views', path.join(__dirname, 'pages'));
@@ -20,10 +19,10 @@ app.use(express.json());
 // Rotas
 app.use('/api', apiRoutes);
 
-// Servir páginas estáticas
+
 app.use(express.static(path.join(__dirname, 'pages')));
 
-// Iniciar o servidor
+
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
